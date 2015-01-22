35c3039f5eba7bb80c1d6c9fcb67fe3c0bf32bb5
supriyak <rishabh.332@gmail.com>
2015-01-22 18:57:58 +0530

    paperclip installation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :bookname, :rating
+  attr_accessible :author, :bookname, :rating ,:author_image
+  has_attached_file :author_image, :styles => {:small => "20x200"}
 end
